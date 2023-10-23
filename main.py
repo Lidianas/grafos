1c9bd7cf799ab0be5fc4042363a2f93637f77b28
Lidiana <lidianasouza101@gmail.com>
2023-10-22 22:12:44 -0300

    alterações em arquivos internos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import list_graph
 import weighted_list_graph
 class main():
 
-    inputFile = "grafos/exemplo_1.txt"
+    inputFile = "grafos/grafo_W_1.txt"
 
     #Escolher o tipo de representação para distâncias
     # 1 - Vetor
